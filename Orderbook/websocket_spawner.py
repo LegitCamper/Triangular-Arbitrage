2381f97e8ec239f8caff16f6e9dee42da24f3854
sawyer bristol <sawyerbristol@gmail.com>
2023-01-14 20:00:55 -0700

    working on algo

--- Orderbook/websocket_spawner.py
+++ Orderbook/websocket_spawner.py
@@ -1,12 +1,14 @@
+from itertools import combinations
 from kucoin.client import Market
 from threading import Thread
+import json
 import subprocess
 import os
 
 
 default_currancy = "USDT"
+stable_coins = ["USDT", "TUSD", "BUSD", "USDC", "DAI"] #"PAX"
 kucoin_fees = 0.1
-kucoin_arb_path = []
 
 
 client = Market(url="https://api.kucoin.com")
@@ -25,7 +27,85 @@ def get_all_coins():
     coins = []
     for i in client.get_currencies():
         coins.append(i["currency"])
-    return coins
+    return coin
+
+def find_tri_arb_path():
+    pairs_in_Results = os.listdir(f"{os.getcwd()}/Results/")
+    
+    for combo in combinations(pairs_in_Results, 3):
+        with open(f"{os.getcwd()}/Results/{combo[0]}") as f:
+            pair1_orderbook = json.loads(f.read())
+        with open(f"{os.getcwd()}/Results/{combo[1]}") as f:
+            pair2_orderbook = json.loads(f.read())
+        with open(f"{os.getcwd()}/Results/{combo[2]}") as f:
+            pair3_orderbook = json.loads(f.read())
+
+        pair1 = list(pair1_orderbook.keys())[0]
+        pair2 = list(pair2_orderbook.keys())[0]
+        pair3 = list(pair3_orderbook.keys())[0]
+
+        # Needs to pass two requirements:
+        # 1) 2 pairs need to have stable coins.
+        # 2) I need to be able to chain together the 3 pairs BTC->ETH->KCS->BTC
+        
+        coin_counter = 0
+        for i in stable_coins:
+            if (par1.split("-")[0] == i and part1.split()
+
+
+                )
+
+        if (pair1.split("-")[0] == pair2.split("-")[0] or 
+            pair1.split("-")[0] == pair2.split("-")[1] or 
+            pair1.split("-")[1] == pair2.split("-")[0] or 
+            pair1.split("-")[1] == pair2.split("-")[1] and
+            pair2.split("-")[0] == pair3.split("-")[0] or
+            pair2.split("-")[0] == pair3.split("-")[1] or
+            pair2.split("-")[1] == pair3.split("-")[0] or
+            pair2.split("-")[1] == pair3.split("-")[1]):
+
+            
+
+
+
+            for i in stable_coins:
+                if i != pair1.split("-")[0]:
+                    Pairs_okay = False
+                    break
+                else:
+                    Pairs_okay = True
+
+
+
+
+
+
+                    )
+
+        Pairs_okay = False 
+        if Pairs_okay:
+
+        pair1_asks = pair1_orderbook[pair1]['asks']
+        pair1_bids = pair1_orderbook[pair1]['bids']
+        pair2_asks = pair2_orderbook[pair2]['asks']
+        pair2_bids = pair2_orderbook[pair2]['asks']
+
+        # MANA-BTC
+        # Price in BTC, Amount in MANA 
+        # Bid is the price buyers are ready to buy at
+        # Ask id the price sellers are ready to sell at
+
+
+
+    # Logic to determine if a path is availibe
+
+
+def find_arb():
+    while True:
+        if find_tri_arb_path():
+            pass
+            # Print results to a loging api - work on actually trading later!!
+            # Logic to execute trade with path
 
 
 def thread_the_process(counter, coin_pairs_string):
@@ -33,19 +113,7 @@ def thread_the_process(counter, coin_pairs_string):
     p.wait()
 
 
-def find_tri_arb_path():
-    pass
-    # Logic to determine if a path is availibe
-
-
-def find_arb():
-    if find_tri_arb_path():
-        pass
-        # Logic to execute trade with path
-
-
 if __name__ == "__main__":
-    # Ensures Results folder exists
     if not os.path.exists(f"{os.getcwd()}/Results/"):
         os.makedirs(f"{os.getcwd()}/Results/")
 
@@ -62,8 +130,9 @@ if __name__ == "__main__":
         coin_pairs_string = coin_pairs_string[0:-1]
         
         # Runs websockets in a thread loop forever
-        Thread(target=thread_the_process, args=(counter, coin_pairs_string)).start()
+        #Thread(target=thread_the_process, args=(counter, coin_pairs_string)).start()
 
-        # Determines if there is an Arbitrage
-
+    # Determines if there is an Arbitrage
+    #Thread(target=find_arb, args=()).Start()    
+    find_tri_arb_path()
 
