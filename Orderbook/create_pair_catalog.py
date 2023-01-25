e7c941aa207ed5b89a225dbc116fe2664afbae7d
LegitCamper <sawyerbristol@gmail.com>
2023-01-25 11:16:05 -0700

    fixing catalog.py
    it still has multiple stable coins in the list so it could limit the number of actual pairs we are watching, but it could be a strength

--- Orderbook/create_pair_catalog.py
+++ Orderbook/create_pair_catalog.py
@@ -14,7 +14,6 @@ client = Market(url="https://api.kucoin.com")
 
 def get_tradable_coin_pairs():
     coin_pairs = []
-    count = 0
     for i in client.get_symbol_list():
         if i["enableTrading"]:
             coin_pairs.append(i["symbol"])
@@ -22,8 +21,6 @@ def get_tradable_coin_pairs():
 
 
 def create_catalog():
-    pairs_in_Results = os.listdir(f"{os.getcwd()}/Results/")
-    
     json_output = []
 
     for combo in combinations_with_replacement(get_tradable_coin_pairs(), 3):
@@ -36,52 +33,25 @@ def create_catalog():
         # 2) the stable coins must only be in the first and third pair
         # 3) I need to be able to chain together the 3 pairs USDT-BTC->BTC-ETH->ETH-USDT
         
-        stable_coins_check = False
-        right_order_check = False
-        pairs_chain_check = False
         pairs_list = [pair1[0], pair1[1], pair2[0], pair2[1], pair3[0], pair3[1]]
 
         # Ensures the stable coin only occurs twice and the stable coin is not in the middle pair
-        num_of_stable_coins = 0
         for i in stable_coins:
             if (pairs_list.count(i) == 2 and 
                 i != pairs_list[2] and i != pairs_list[3]):
-                num_of_stable_coins = num_of_stable_coins + 1
                 stable_coin_in_pairs = i
-                stable_coins_check = True
-                right_order_check = True
-            else:
-                continue
-    
-        if num_of_stable_coins > 1:
-            continue
 
-        # Skips other checks if stable_coins_check fails
-        if not stable_coins_check:
-            continue
+                # Ensures the beginning and end of pairs_list are both stable coins
+                if (stable_coin_in_pairs == pairs_list[0] or stable_coin_in_pairs == pairs_list[1] and
+                    stable_coin_in_pairs == pairs_list[4] or stable_coin_in_pairs == pairs_list[5]):
 
-        # Ensures the beginning and end of pairs_list are both stable coins
-        try: # Catches exception that gets raised when stable_coin_in_pairs is not defined
-            if (stable_coin_in_pairs == pairs_list[0] or stable_coin_in_pairs == pairs_list[1] and
-                stable_coin_in_pairs == pairs_list[4] or stable_coin_in_pairs == pairs_list[5]):
-                right_order_check = True
-            else:
-                continue
-        except:
-            continue
+                    # Ensure the pairs can chain together
+                    if (pairs_list.count(pairs_list[0]) == 2 and
+                        pairs_list.count(pairs_list[1]) == 2 and
+                        pairs_list.count(pairs_list[2]) == 2 and
+                        pairs_list.count(pairs_list[3]) == 2):
 
-        # Ensure the pairs can chain together
-        if (pairs_list.count(pairs_list[0]) == 2 and
-            pairs_list.count(pairs_list[1]) == 2 and
-            pairs_list.count(pairs_list[2]) == 2 and
-            pairs_list.count(pairs_list[3]) == 2):
-            pairs_chain_check = True
-        else:
-            continue
-
-        if stable_coins_check and right_order_check and pairs_chain_check:
-            json_output.append(pairs_list)
-        
+                        json_output.append(pairs_list)
 
     json.dump(json_output, pairs_catalog)
 
