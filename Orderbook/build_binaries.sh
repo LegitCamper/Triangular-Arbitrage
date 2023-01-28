340d1d35d1f88c461666655a2f67ad9a6dbc2c7c
sawyer bristol <sawyerbristol@gmail.com>
2023-01-28 16:39:18 -0700

    reverting to gcc

--- Orderbook/build_binaries.sh
+++ Orderbook/build_binaries.sh
@@ -4,9 +4,9 @@
 # Have nuitka installed via pip
 
 # Main Files
-nuitka3 --quiet --remove-output --output-dir=bin --clang websockets.py
-nuitka3 --quiet --remove-output --output-dir=bin --clang websocket_spawner.py
-nuitka3 --quiet --remove-output --output-dir=bin --clang find_triangular_arbitrage.py
+nuitka3 --quiet --remove-output --output-dir=bin websockets.py
+nuitka3 --quiet --remove-output --output-dir=bin websocket_spawner.py
+nuitka3 --quiet --remove-output --output-dir=bin find_triangular_arbitrage.py
 
 # Helper files
-nuitka3 --quiet --remove-output --output-dir=bin --clang create_valid_pairs_catalog.py
+nuitka3 --quiet --remove-output --output-dir=bin create_valid_pairs_catalog.py
