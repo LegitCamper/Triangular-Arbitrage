c54811e57bf15ec7b99e8a3fbd87c83f9f137d35
sawyer bristol <sawyerbristol@gmail.com>
2023-01-28 15:59:10 -0700

    fixing catalog

--- Orderbook/bin/run.sh
+++ Orderbook/bin/run.sh
@@ -1,10 +1,7 @@
 # Runs the Triangular Arbitrage programs
 
-
 # Generates catalog
-./create_valid_pairs_catalog.bin & 
-
-sleep 60
+./create_valid_pairs_catalog.bin
 
 # Downloads symbols        # Finds Arbitrages
 ./websocket_spawner.bin & ./find_triangular_arbitrage.bin && fg
