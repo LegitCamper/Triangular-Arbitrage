37f8fd0813714dc1f29444446311ba04e3caf08a
sawyer bristol <sawyerbristol@gmail.com>
2023-01-29 18:09:15 -0700

    removing the need to regen catalog

--- Orderbook/bin/run.sh
+++ Orderbook/bin/run.sh
@@ -1,7 +1,7 @@
 # Runs the Triangular Arbitrage programs
 
 # Generates catalog
-./create_valid_pairs_catalog.bin
+#./create_valid_pairs_catalog.bin
 
 # Downloads symbols        # Finds Arbitrages
 ./websocket_spawner.bin & ./find_triangular_arbitrage.bin && fg
