75d92fc8e3be7b4f55c46238d8fa634f22ac696e
sawyer bristol <sawyerbristol@gmail.com>
2023-03-18 19:15:38 -0600

    updating catalog

--- Kucoin-Rust/src/main.rs
+++ Kucoin-Rust/src/main.rs
@@ -1,18 +1,22 @@
 use core::future::poll_fn;
 use rand::prelude::*;
-
 use std::env;
 use std::fmt::Display;
 use std::fs::{read_to_string, remove_file, File};
+//use std::marker::Tuple;
 use std::os::unix::process;
 use std::path::Path;
 extern crate libc;
 use duration_string::DurationString;
+use itertools::Itertools;
 use reqwest::header::USER_AGENT;
 use serde::{Deserialize, Serialize};
+use serde_this_or_that::as_f64;
 use std::ffi::CString;
 use std::time::{SystemTime, UNIX_EPOCH};
 use url::Url;
+
+const STABLE_COINS: [&str; 1] = ["USDT"]; // "TUSD", "BUSD", "USDC", "DAI"
 
 fn cwd_plus_path(path: String) -> String {
     let cwd = env::current_dir()
@@ -57,9 +61,8 @@ struct KucoinRequest {
 }
 
 async fn kucoin_rest_api(data: KucoinRequest, endpoint: &str) -> String {
-    println!("{}", data.get_or_post);
     let (api_key, api_passphrase, api_key_version, api_timestamp) = get_api_keys();
-    println!("{}", api_passphrase);
+
     let json = serde_json::to_string(&data).unwrap();
 
     let base_url: Url =
@@ -83,7 +86,6 @@ async fn kucoin_rest_api(data: KucoinRequest, endpoint: &str) -> String {
             .text()
             .await
             .expect("failed to get payload");
-        println!("{}", res);
         res
     } else if data.get_or_post == "post" {
         client
@@ -103,8 +105,56 @@ async fn kucoin_rest_api(data: KucoinRequest, endpoint: &str) -> String {
     }
 }
 
+#[allow(non_snake_case)]
+#[allow(dead_code)]
+#[derive(Debug, Deserialize)]
+struct KucoinCoins {
+    symbol: String,
+    symbolName: String,
+    #[serde(deserialize_with = "as_f64")]
+    buy: f64,
+    #[serde(deserialize_with = "as_f64")]
+    sell: f64,
+    #[serde(deserialize_with = "as_f64")]
+    changeRate: f64,
+    #[serde(deserialize_with = "as_f64")]
+    changePrice: f64,
+    #[serde(deserialize_with = "as_f64")]
+    high: f64,
+    #[serde(deserialize_with = "as_f64")]
+    low: f64,
+    #[serde(deserialize_with = "as_f64")]
+    vol: f64,
+    #[serde(deserialize_with = "as_f64")]
+    volValue: f64,
+    #[serde(deserialize_with = "as_f64")]
+    last: f64,
+    #[serde(deserialize_with = "as_f64")]
+    averagePrice: f64,
+    #[serde(deserialize_with = "as_f64")]
+    takerFeeRate: f64,
+    #[serde(deserialize_with = "as_f64")]
+    makerFeeRate: f64,
+    #[serde(deserialize_with = "as_f64")]
+    takerCoefficient: f64,
+    #[serde(deserialize_with = "as_f64")]
+    makerCoefficient: f64,
+}
+
+#[derive(Debug, Deserialize)]
+#[allow(dead_code)]
+struct KucoinCoinsTime {
+    ticker: Vec<KucoinCoins>,
+}
+
+#[derive(Debug, Deserialize)]
+#[allow(dead_code)]
+struct KucoinCoinsCode {
+    data: KucoinCoinsTime,
+}
+
 // Cahnge the output type
-fn get_tradable_coin_pairs() -> String {
+async fn get_tradable_coin_pairs() -> KucoinCoinsCode {
     let mut rng = rand::thread_rng();
     let kucoin_request = KucoinRequest {
         get_or_post: "get".to_string(),
@@ -116,13 +166,80 @@ fn get_tradable_coin_pairs() -> String {
         order_symbol: "None".to_string(),
         order_type: "None".to_string(),
     };
-    kucoin_rest_api(kucoin_request, "/api/v1/market/allTickers");
-    //println!("{}", none);
-    "ffs".to_string()
+    let kucoin_request_string = kucoin_rest_api(kucoin_request, "/api/v1/market/allTickers").await;
+    serde_json::from_str(&kucoin_request_string.as_str()).expect("JSON was not well-formatted")
 }
 
-fn valid_combinations_3() {
-    // make all possible combinations of 3 coins here
+fn has_two_occurrences(vec: &Vec<String>, string: &str) -> bool {
+    let mut count = 0;
+    for s in vec {
+        if s == string {
+            count += 1;
+        }
+        if count > 2 {
+            return false;
+        }
+    }
+    count == 2
+}
+
+fn validate_combination(pairs_list: &Vec<String>) -> bool {
+    let pairs_list_len = pairs_list.len();
+
+    // ensures the pairs can chain together
+    let mut chainable: bool = false;
+    for i in pairs_list.iter() {
+        if has_two_occurrences(&pairs_list, i) {
+            chainable = true
+        } else {
+            chainable = false;
+            break;
+        }
+    }
+
+    // ensures first and last pair have a stable coin
+    let mut stable: bool = false;
+    for i in STABLE_COINS.iter() {
+        let si = i.to_string();
+        if si == pairs_list[0]
+            || si == pairs_list[1] && si == pairs_list[pairs_list_len - 2]
+            || si == pairs_list[pairs_list_len - 1]
+        {
+            stable = true;
+        }
+    }
+    if stable == true && chainable == true {
+        true
+    } else {
+        false
+    }
+}
+
+// make all possible combinations of 3 coins here
+fn valid_combinations_3(coin_pairs: Vec<String>) {
+    let mut valid_combinations: Vec<Vec<String>> = Vec::new();
+    for pair1 in coin_pairs.iter() {
+        for pair2 in coin_pairs.iter() {
+            for pair3 in coin_pairs.iter() {
+                let pair1: Vec<&str> = pair1.split("-").collect();
+                let pair2: Vec<&str> = pair2.split("-").collect();
+                let pair3: Vec<&str> = pair3.split("-").collect();
+
+                let pairs_list = vec![
+                    pair1[0].to_string(),
+                    pair1[1].to_string(),
+                    pair2[0].to_string(),
+                    pair2[1].to_string(),
+                    pair3[0].to_string(),
+                    pair3[0].to_string(),
+                ];
+                if validate_combination(&pairs_list) == true {
+                    valid_combinations.push(pairs_list)
+                }
+            }
+        }
+    }
+    print!("{:?}", valid_combinations)
 }
 
 fn valid_combinations_4() {
@@ -133,7 +250,7 @@ fn vailid_combinations_5() {
     // make all possible combinations of 5 coins here
 }
 
-fn create_valid_pairs_catalog() {
+async fn create_valid_pairs_catalog() {
     // Deletes old pairs catalog and makes new file to write to
     let json_file_path = cwd_plus_path("/Triangular_pairs.catalog".to_string());
     if Path::new(&json_file_path).exists() {
@@ -142,26 +259,18 @@ fn create_valid_pairs_catalog() {
     let json_file = Path::new(&json_file_path).exists();
     let catalog_output: Vec<Vec<String>> = Vec::new(); // Holds the outputs of all Triangular pairs for printing
 
-    let coin_pairs = get_tradable_coin_pairs();
-    println!("{:?}", coin_pairs);
+    let coin_pairs_struct = get_tradable_coin_pairs().await;
+    let mut coin_pairs: Vec<String> = Vec::new();
+
+    for i in coin_pairs_struct.data.ticker.iter() {
+        coin_pairs.push(i.symbol.clone());
+    }
+
+    valid_combinations_3(coin_pairs);
 }
 
-fn main() {
-    let fifo_path: String = cwd_plus_path("/trades.pip".to_string());
-    println! {"{}", fifo_path} // ensure this is correct
-                               //create_valid_pairs_catalog()
-    let mut rng = rand::thread_rng();
-    let kucoin_request = KucoinRequest {
-        get_or_post: "get".to_string(),
-        get_symbols: true,
-        client_id: rng.gen_range(1000..99999), // Generates new random client id
-        order_amount: 0.0,
-        order_price: 0.0,
-        order_side: "None".to_string(),
-        order_symbol: "None".to_string(),
-        order_type: "None".to_string(),
-    };
-    let l = poll_fn(kucoin_rest_api(kucoin_request, "/api/v1/market/allTickers"));
-    println!("{}", l);
+#[tokio::main] // allows main to be async
+async fn main() {
+    let fifo_path: String = cwd_plus_path("/trades.pipe".to_string());
+    create_valid_pairs_catalog().await;
 }
-
