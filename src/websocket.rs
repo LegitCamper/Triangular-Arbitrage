972000e25c06f50c013a35f7c7f8c37bbd9d070c
sawyer bristol <sawyerbristol@gmail.com>
2024-01-05 20:27:02 -0700

    requests fail

--- src/websocket.rs
+++ src/websocket.rs
@@ -2,9 +2,9 @@ use binance::account::*;
 use binance::api::*;
 use binance::rest_model::{Asks, Bids, OrderBook};
 use binance::rest_model::{OrderSide, OrderType, TimeInForce};
-use binance::userstream::*;
+// use binance::userstream::*;
 use binance::websockets::*;
-use binance::ws_model::{CombinedStreamEvent, WebsocketEvent, WebsocketEventUntag};
+use binance::ws_model::{CombinedStreamEvent, WebsocketEventUntag}; // WebsocketEvent
 use log::{error, info, warn};
 use std::{
     collections::HashMap,
@@ -156,77 +156,81 @@ async fn user_stream_websocket(
     mut orders: UnboundedReceiver<Vec<func::OrderStruct>>,
 ) -> JoinHandle<()> {
     tokio::spawn(async move {
-        let keep_running = AtomicBool::new(true); // Used to control the event loop
-        let user_stream: UserStream = Binance::new(Some(key.key.clone()), Some(key.secret.clone()));
+        // let keep_running = AtomicBool::new(true); // Used to control the event loop
+        // let user_stream: UserStream = Binance::new(Some(key.key.clone()), Some(key.secret.clone()));
         let account: Account = Binance::new(Some(key.key), Some(key.secret));
 
-        if let Ok(answer) = user_stream.start().await {
-            let listen_key = answer.listen_key;
+        // if let Ok(answer) = user_stream.start().await {
+        //     let listen_key = answer.listen_key;
 
-            let mut web_socket: WebSockets<'_, WebsocketEvent> =
-                WebSockets::new(|event: WebsocketEvent| {
-                    if let WebsocketEvent::OrderUpdate(trade) = event {
-                        info!(
-                            "Symbol: {}, Side: {:?}, Price: {}, Execution Type: {:?}",
-                            trade.symbol, trade.side, trade.price, trade.execution_type
-                        );
-                        // orders.send(trade).expect("Failed to send trade")
-                    };
-                    Ok(())
-                });
+        //     let mut web_socket: WebSockets<'_, WebsocketEvent> =
+        //         WebSockets::new(|event: WebsocketEvent| {
+        //             if let WebsocketEvent::OrderUpdate(trade) = event {
+        //                 // info!(
+        //                 //     "Symbol: {}, Side: {:?}, Price: {}, Execution Type: {:?}",
+        //                 //     trade.symbol, trade.side, trade.price, trade.execution_type
+        //                 // );
+        //                 // orders.send(trade).expect("Failed to send trade")
+        //             };
+        //             Ok(())
+        //         });
 
-            web_socket.connect(&listen_key).await.unwrap(); // check error
+        //     web_socket.connect(&listen_key).await.unwrap(); // check error
 
-            // listens for orders and passes them to the user websocket
-            if let Some(orders) = orders.recv().await {
-                info!("Received orders: {:?}", orders);
-                // TODO: catch errors and reverse orders that fail in 2/3 or 3/3
-                for order in orders.iter() {
-                    let symbol = order.symbol.clone();
-                    match order.side {
-                        func::ArbOrd::Buy(_, _) => {
-                            let limit_buy = OrderRequest {
-                                symbol: symbol.to_string(),
-                                quantity: Some(order.size),
-                                price: Some(order.price),
-                                order_type: OrderType::Limit,
-                                side: OrderSide::Buy,
-                                time_in_force: Some(TimeInForce::FOK),
-                                ..OrderRequest::default()
-                            };
-                            match account.place_order(limit_buy).await {
-                                Ok(answer) => info!("{:?}", answer),
-                                Err(e) => error!("Error: {e}"),
-                            }
-                        }
-                        func::ArbOrd::Sell(_, _) => {
-                            let limit_sell = OrderRequest {
-                                symbol: symbol.to_string(),
-                                quantity: Some(order.size),
-                                price: Some(order.price),
-                                order_type: OrderType::Limit,
-                                side: OrderSide::Sell,
-                                time_in_force: Some(TimeInForce::FOK),
-                                ..OrderRequest::default()
-                            };
-                            match account.place_order(limit_sell).await {
-                                Ok(answer) => info!("{:?}", answer),
-                                Err(e) => error!("Error: {e}"),
-                            }
-                        }
-                    }
+        // listens for orders and passes them to the user websocket
+        if let Some(orders) = orders.recv().await {
+            info!("Received orders: {:?}", orders);
+            // TODO: catch errors and reverse orders that fail in 2/3 or 3/3
+            for order in orders.iter() {
+                let symbol = order.symbol.clone();
+                let limit = match order.side {
+                    func::ArbOrd::Buy(_, _) => OrderRequest {
+                        symbol: symbol.to_string(),
+                        quantity: Some(order.size),
+                        price: Some(order.price),
+                        order_type: OrderType::Limit,
+                        quote_order_qty: None,
+                        side: OrderSide::Buy,
+                        time_in_force: Some(TimeInForce::FOK),
+                        // recv_window: Some(6000),
+                        new_client_order_id: None,
+                        stop_price: None,
+                        iceberg_qty: None,
+                        new_order_resp_type: None,
+                        ..OrderRequest::default()
+                    },
+                    func::ArbOrd::Sell(_, _) => OrderRequest {
+                        symbol: symbol.to_string(),
+                        quantity: Some(order.size),
+                        price: Some(order.price),
+                        order_type: OrderType::Limit,
+                        quote_order_qty: None,
+                        side: OrderSide::Sell,
+                        time_in_force: Some(TimeInForce::FOK),
+                        // recv_window: Some(6000),
+                        new_client_order_id: None,
+                        stop_price: None,
+                        iceberg_qty: None,
+                        new_order_resp_type: None,
+                        ..OrderRequest::default()
+                    },
+                };
+                match account.place_order(limit).await {
+                    Ok(answer) => info!("{:?}", answer),
+                    Err(e) => error!("Error: {e}"),
                 }
-                // sleep to ensure order goes through once at a time
-                sleep(Duration::from_secs(10)).await;
             }
-            if let Err(e) = web_socket.event_loop(&keep_running).await {
-                println!("Error: {e}");
-            }
-            user_stream.close(&listen_key).await.unwrap();
-            web_socket.disconnect().await.unwrap();
-            println!("Userstrem closed and disconnected");
-        } else {
-            println!("Not able to start an User Stream (Check your API_KEY)");
+            // sleep to ensure order goes through once at a time
+            sleep(Duration::from_secs(10)).await;
         }
+        // if let Err(e) = web_socket.event_loop(&keep_running).await {
+        //     println!("Error: {e}");
+        // }
+        //     user_stream.close(&listen_key).await.unwrap();
+        //     web_socket.disconnect().await.unwrap();
+        //     println!("Userstrem closed and disconnected");
+        // } else {
+        //     println!("Not able to start an User Stream (Check your API_KEY)");
+        // }
     })
 }
